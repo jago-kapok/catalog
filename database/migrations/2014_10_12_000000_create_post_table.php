2a91c3451a31c211e1bb1304032f7fc89723993d
dikiun <dikiandikiawan@mhs.unhasy.ac.id>
2024-10-10 15:47:14 +0700

    Update Routes and Add database Migration

---
+++ database/migrations/2014_10_12_000000_create_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('kategori_id');
+            $table->string('judul');
+            $table->longText('deskripsi');
+            $table->integer('created_by');
+            $table->timestamps();
+            $table->datetimes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,20 @@ Route::get('/home', function () {
 })->name('admin');
 
 Route::get('/', [App\Http\Controllers\PostController::class, 'landing'])->name('landing');
-Route::get('/post/{id}', [App\Http\Controllers\PostController::class, 'post']);
+Route::get('/show-post/{id}', [App\Http\Controllers\PostController::class, 'post']);
+
+Route::prefix('post')->middleware('auth')->group(function () {
+    Route::get('/', [App\Http\Controllers\PostController::class, 'index'])->name('post');
+    Route::get('/form/{id?}', [App\Http\Controllers\PostController::class, 'form'])->name('post-form');
+    Route::post('/store', [App\Http\Controllers\PostController::class, 'store'])->name('post-store');
+    Route::post('/destroy', [App\Http\Controllers\PostController::class, 'destroy'])->name('post-destroy');
+});
+
+Route::prefix('kategori')->middleware('auth')->group(function () {
+    Route::get('/', [App\Http\Controllers\KategoriController::class, 'index'])->name('kategori');
+    Route::get('/form/{id?}', [App\Http\Controllers\KategoriController::class, 'form'])->name('kategori-form');
+    Route::post('/store', [App\Http\Controllers\KategoriController::class, 'store'])->name('kategori-store');
+    Route::post('/destroy', [App\Http\Controllers\KategoriController::class, 'destroy'])->name('kategori-destroy');
+});
 
 Auth::routes();
